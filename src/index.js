03ced402baa27a1f02673ce2aa502bf4f2cf45fd
jonah <jonahkf@gmail.com>
2024-12-22 15:42:26 +0900

    COMPLETE: HashMap Project

--- src/index.js
+++ src/index.js
@@ -359,19 +359,28 @@ class HashMap {
   }
 }
 
-const test = new HashMap();
-test.set("apple", "red");
-test.set("banana", "yellow");
-test.set("carrot", "orange");
-test.set("dog", "brown");
-test.set("elephant", "gray");
-test.set("frog", "green");
-test.set("grape", "purple");
-test.set("hat", "black");
-test.set("ice cream", "white");
-test.set("jacket", "blue");
-test.set("kite", "pink");
-test.set("lion", "golden");
+// Test Code Below
+// const test = new HashMap();
+// test.set("apple", "red");
+// test.set("banana", "yellow");
+// test.set("carrot", "orange");
+// test.set("dog", "brown");
+// test.set("elephant", "gray");
+// test.set("frog", "green");
+// test.set("grape", "purple");
+// test.set("hat", "black");
+// test.set("ice cream", "white");
+// test.set("jacket", "blue");
+// test.set("kite", "pink");
+// test.set("lion", "golden");
 // console.log(test.keys());
 // console.log(test.values());
-console.log(test.entries());
+// console.log(test.entries());
+// console.log(test.capacity);
+// console.log(test.size / test.capacity);
+// test.set("moon", "silver");
+// console.log(test.capacity);
+// console.log(test.size / test.capacity);
+// console.log(test.keys());
+// console.log(test.values());
+// console.log(test.entries());
